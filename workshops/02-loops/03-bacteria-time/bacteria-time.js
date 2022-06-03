8259fac0a201f4756047565cc6813b306de63533
su-de-sh <sudesh7443@gmail.com>
2022-06-03 13:53:45 +0545

    loops problem no 3 solved

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function bacteriaTime(currentNum, targetNum) {
+  if (currentNum > targetNum) return "targetNum must be larger than currentNum";
+  else {
+    let count = 0;
+    while (targetNum > currentNum) {
+      currentNum *= 2;
+      count += 1;
+    }
+    return count * 20;
+  }
+}
