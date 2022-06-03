652475da6e2f70f7765369668e7e0f6b5bc17f75
su-de-sh <sudesh7443@gmail.com>
2022-06-03 13:28:07 +0545

    loops problem no 1 solved

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function onlyOdds(num) {
+  if (num < 0) return 0;
+  else {
+    sum = 0;
+    for (let i = 0; i <= num; i++) {
+      rem = i % 2;
+      if (rem === 1) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
+}
