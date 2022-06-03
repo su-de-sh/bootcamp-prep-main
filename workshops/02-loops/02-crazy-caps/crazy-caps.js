da8db60cd21cb7535ec9ec1483f57ff6466cf1b2
su-de-sh <sudesh7443@gmail.com>
2022-06-03 13:37:43 +0545

    loops second problem solved

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function crazyCaps(myString) {
+  let finalString = "";
+  for (let i = 0; i <= myString.length - 1; i++) {
+    rem = i % 2;
+    if (rem === 1) {
+      finalString += myString[i].toUpperCase();
+    } else {
+      finalString += myString[i];
+    }
+  }
+  return finalString;
+}
