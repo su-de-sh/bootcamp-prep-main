aebdd0ef9b7647aa4dd1191d13bbfd6ffdc38193
su-de-sh <sudesh7443@gmail.com>
2022-06-03 14:29:44 +0545

    loops 05 problem solved

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+function mySlice(originalString, startIdx, endIdx) {
+  finalString = "";
+  if (startIdx && endIdx) {
+    for (let i = startIdx; i <= endIdx - 1; i++) {
+      finalString += originalString[i];
+    }
+  } else if (startIdx) {
+    for (let i = startIdx; i <= originalString.length - 1; i++) {
+      finalString += originalString[i];
+    }
+  } else if (endIdx) {
+    for (let i = 0; i <= endIdx; i++) {
+      finalString += originalString[i];
+    }
+  } else {
+    finalString = originalString;
+  }
+
+  return finalString;
+}
