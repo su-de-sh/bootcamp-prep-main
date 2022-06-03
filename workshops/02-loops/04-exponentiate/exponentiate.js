172978f88410fc747f42908caf332c7aa55dfc59
su-de-sh <sudesh7443@gmail.com>
2022-06-03 14:15:51 +0545

    loops problem 4 solved

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function exponentiate(base, power) {
+  if (power < 0) return 0;
+  else {
+    let product = 1;
+    for (let i = 1; i <= power; i++) {
+      product *= base;
+    }
+    return product;
+  }
+}
