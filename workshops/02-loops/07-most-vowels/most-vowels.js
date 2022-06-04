5fbe1129bd2cb0c2c9fc6ddfe5358d4214f9133a
su-de-sh <sudesh7443@gmail.com>
2022-06-04 20:46:34 +0545

    loop 07 problem solved

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,36 @@
 // YOUR CODE BELOW
+function mostVowels(string) {
+  lastLetter = string[string.length - 1];
+
+  if (lastLetter === ".") {
+    string = string.slice(0, string.length - 1);
+  }
+  let vowels = "aeiou";
+  let words = string.split(" ");
+
+  let wordIndex = 0;
+  let currentVowelCount = 0;
+  let maxVowelCount = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+
+    for (let j = 0; j < word.length; j++) {
+      for (let k = 0; k < vowels.length; k++) {
+        if (word[j] === vowels[k]) {
+          currentVowelCount++;
+        }
+      }
+    }
+
+    if (currentVowelCount > maxVowelCount) {
+      maxVowelCount = currentVowelCount;
+
+      wordIndex = i;
+    }
+    currentVowelCount = 0;
+  }
+
+  if (maxVowelCount === 0) return "";
+  return words[wordIndex];
+}
