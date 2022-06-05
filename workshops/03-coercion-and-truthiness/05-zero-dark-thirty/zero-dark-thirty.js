55dfd43a5cd780713b69ae63a49fd79ea9908ebf
su-de-sh <sudesh7443@gmail.com>
2022-06-05 12:30:00 +0545

    coercion problem no 5 solved

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function zeroDarkThirty(num) {
+  if (num === 0) return NaN;
+  let strnum = String(num);
+  let newStr = "";
+
+  for (let i = 0; i < strnum.length; i++) {
+    if (strnum[i] === "0") {
+      continue;
+    } else {
+      newStr += strnum[i];
+    }
+  }
+  return Number(newStr);
+}
