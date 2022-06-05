4da76b9beaa71958dcd38610b447aefcb990dd27
su-de-sh <sudesh7443@gmail.com>
2022-06-05 10:58:45 +0545

    coercion problem no 2 solved

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+function isTruthy(arg1) {
+  if (arg1) {
+    return true;
+  } else {
+    if (arg1 === false) {
+      return "The boolean value false is falsey";
+    }
+    if (arg1 === null) {
+      return "The null value is falsey";
+    }
+    if (arg1 === 0) {
+      return "The number 0 is falsey (the only falsey number)";
+    }
+    if (arg1 === undefined) {
+      return "undefined is falsey";
+    }
+    if (arg1 === "") {
+      return "The empty string is falsey (the only falsey string)";
+    }
+  }
+}
