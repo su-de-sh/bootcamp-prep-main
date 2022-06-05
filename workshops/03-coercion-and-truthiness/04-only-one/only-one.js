fd706e1666627d630ae194edd713cc424a8cc3d9
su-de-sh <sudesh7443@gmail.com>
2022-06-05 12:24:23 +0545

    coercion problem no 4 solved

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// function onlyOne(arg1, arg2, arg3) {
+//   count = 0;
+//   if (arg1) count++;
+//   if (arg2) count++;
+//   if (arg3) count++;
+//   if (count === 1) {
+//     return true;
+//   } else return false;
+// }
+
+function onlyOne(arg1, arg2, arg3) {
+  if (
+    (arg1 && !arg2 && !arg3) ||
+    (!arg1 && arg2 && !arg3) ||
+    (!arg1 && !arg2 && arg3)
+  )
+    return true;
+  else return false;
+}
