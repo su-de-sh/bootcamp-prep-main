6082efdc8ebc9bbe29d7deab8632702101b58776
su-de-sh <sudesh7443@gmail.com>
2022-06-05 11:16:45 +0545

    problem no 3 of coercion resolved

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,8 +1,14 @@
 // YOUR CODE BELOW
 function myOr(arg1, arg2, arg3) {
-  return arg1 || arg2 || arg3;
+  if (!!arg1) return arg1;
+  if (!!arg2) return arg2;
+  if (!!arg3) return arg3;
+  return arg3;
 }
 
 function myAnd(arg1, arg2, arg3) {
-  return arg1 && arg2 && arg3;
+  if (!arg1) return arg1;
+  if (!arg2) return arg2;
+  if (!arg3) return arg3;
+  return arg3;
 }
