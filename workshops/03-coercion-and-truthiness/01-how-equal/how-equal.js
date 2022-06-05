10bf355b09fba9e3d3c22fd75a39d997f4972347
su-de-sh <sudesh7443@gmail.com>
2022-06-05 10:41:32 +0545

    coercion problem 01 solved

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function howEqual(arg1, arg2) {
+  if (arg1 === arg2) {
+    return "strictly";
+  }
+  if (arg1 == arg2) {
+    return "loosely";
+  }
+
+  return "not equal";
+}
