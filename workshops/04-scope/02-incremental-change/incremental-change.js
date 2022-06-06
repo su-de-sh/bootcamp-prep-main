1059c7e1e7486f6882ad743f280a3fea2aa38e40
su-de-sh <sudesh7443@gmail.com>
2022-06-06 11:13:02 +0545

    scope problem no 2 solved

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,16 @@
 let counter = 0;
+function incrementalChange(increment) {
+  if (increment < 0) {
+    for (let i = 1; i <= -increment; i++) {
+      counter--;
+    }
+    return counter;
+  }
+  for (let i = 1; i <= increment; i++) {
+    counter++;
+  }
+
+  return counter;
+}
 
 // YOUR CODE BELOW
