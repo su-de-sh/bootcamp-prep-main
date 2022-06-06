d93de8a52d325d7f25d1dc6d21abc0e20fa0a031
su-de-sh <sudesh7443@gmail.com>
2022-06-06 12:21:08 +0545

    scope problem 03 and 04 solved

--- workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
+++ workshops/04-scope/03-sum-things-wrong/sum-things-wrong.js
@@ -1,8 +1,12 @@
 let sum = 0;
 
 function sumThingsWrong(num1, num2) {
+  let sum = 0;
   sum += num1;
+
   sum += num2;
 
   return sum;
 }
+
+sumThingsWrong(50, 100);
--- workshops/04-scope/04-sniper-scope/sniper-scope.js
+++ workshops/04-scope/04-sniper-scope/sniper-scope.js
@@ -1,58 +1,53 @@
-let bond = '007';
+let bond = "007";
 
 function oddJob() {
-  let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = bond === "007";
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
-
 
 function goldFinger(bond) {
-  let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = bond === "007";
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
-
 
 function scaramanga(target) {
   target = bond;
 
-  let AgentinScope = (bond === '007')
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = bond === "007";
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
-
 
 function drNo() {
-  let bond = 'Body Double';
+  let bond = "Body Double";
 
-  let AgentinScope = (bond === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = bond === "007";
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
-
 
 function jaws(agent) {
   agent = bond;
-  bond = 'Body Double';
+  bond = "Body Double";
 
-  let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = agent === "007";
+  let prediction = true; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
 
-
 function elChiffre() {
   let agent = bond;
-  bond = 'Body Double';
+  bond = "Body Double";
 
-  let AgentinScope = (agent === '007');
-  let prediction = null; // YOUR BOOLEAN PREDICTION HERE
+  let AgentinScope = agent === "007";
+  let prediction = false; // YOUR BOOLEAN PREDICTION HERE
 
   return AgentinScope === prediction;
 }
