a04b2995408fb6bb17e87f0da7fb19a80e964909
su-de-sh <sudesh7443@gmail.com>
2022-06-07 13:14:11 +0545

    array problem 05 solved

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myUnshift(array, value) {
+  let newArray = [value];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+console.log(myUnshift([1, 2, 3], 0));
