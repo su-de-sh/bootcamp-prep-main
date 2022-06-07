7510598024e80f8b738dbe56b00dd4f573e4bf08
su-de-sh <sudesh7443@gmail.com>
2022-06-07 13:07:29 +0545

    array 04 problem solved

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myReverse(array) {
+  let newArray = [];
+  for (let i = array.length; i > 0; i--) {
+    newArray.push(array.pop());
+  }
+  return newArray;
+}
+console.log(myReverse([1, 2, 3]));
