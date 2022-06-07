4bef3d85d57628e23a30329a79b9aea11136f70f
su-de-sh <sudesh7443@gmail.com>
2022-06-07 11:04:48 +0545

    array 01 problem solved

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function oddCouple(array) {
+  if (array.length === 0) return [];
+  numArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      numArray.push(array[i]);
+      if (numArray.length === 2) break;
+    }
+  }
+  return numArray;
+}
+
+console.log(oddCouple([1, 2, 3, 4, 5]));
