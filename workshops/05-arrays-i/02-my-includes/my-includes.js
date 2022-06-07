05611b66ee3ee95c9de931b683e1e568c18bb99c
su-de-sh <sudesh7443@gmail.com>
2022-06-07 11:13:32 +0545

    array 02 problem solved

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === searchValue) return true;
+  }
+  return false;
+}
+console.log(myIncludes([10, 20, 30], 20));
+console.log(myIncludes(["apples", "bananas", "citrus"], "pears"));
