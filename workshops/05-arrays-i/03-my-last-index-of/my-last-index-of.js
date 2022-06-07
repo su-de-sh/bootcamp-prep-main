13e6099218f9c076f1c905dbaa008e02f52fc6cb
su-de-sh <sudesh7443@gmail.com>
2022-06-07 11:24:23 +0545

    array 03 problem solved

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx = 0) {
+  let myIdx;
+  for (let i = startIdx; i < array.length; i++) {
+    if (array[i] === searchValue) {
+      myIdx = i - startIdx;
+    }
+  }
+  if (myIdx === undefined) return -1;
+  return myIdx;
+}
+console.log(myLastIndexOf(["gee", "gee", "gee", "gee", "baby", "baby"], "gee"));
+console.log(myLastIndexOf(["Tiffany", "Sunny", "Yoona"], "Jessica"));
