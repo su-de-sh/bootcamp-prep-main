745c4024e0a7ea5569be12d859db517720ccd816
su-de-sh <sudesh7443@gmail.com>
2022-06-08 11:46:24 +0545

    arrays-ii 02 problem solved

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function arrayFlattener(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
