20c37533be23a5479fd7e23ec49fdd26f334356d
su-de-sh <sudesh7443@gmail.com>
2022-06-08 13:18:26 +0545

    array-ii problem 04 solved

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function makeGrid(number1, number2) {
+  let newArry = [];
+  for (let i = 1; i <= number2; i++) {
+    let arr = [];
+    for (let j = 1; j <= number1; j++) {
+      arr.push(j);
+    }
+
+    newArry.push(arr);
+  }
+  return newArry;
+}
+
+console.log(makeGrid(3, 4));
