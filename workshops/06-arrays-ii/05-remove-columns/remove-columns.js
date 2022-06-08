ae7eb2db0dac8d819ce66ea647aed48b5d5083cc
su-de-sh <sudesh7443@gmail.com>
2022-06-08 13:49:57 +0545

    array-ii 05 problem solved

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+function removeColumns(originalGrid, numColums) {
+  let newGrid = [];
+  for (let i = 0; i < originalGrid.length; i++) {
+    newGrid.push(originalGrid[i].slice(0, originalGrid[i].length - numColums));
+  }
+  return newGrid;
+}
