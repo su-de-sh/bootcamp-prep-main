259e560485f689541f609c18fe96cf45acac52cf
su-de-sh <sudesh7443@gmail.com>
2022-06-08 14:20:38 +0545

    array-ii problem no 06 solved

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function myJoin(array, separator = ",") {
+  let myStr = "";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === null) array[i] = "";
+    if (i === array.length - 1) myStr += array[i];
+    else myStr += array[i] + separator;
+  }
+  return myStr;
+}
+
+console.log(myJoin(["a", "b", "c"], "+"));
+console.log(myJoin([null, "came, ", "saw, ", "conquered."], "I "));
