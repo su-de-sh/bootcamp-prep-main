648ee3f38837688263b8b1297decb93963c037bc
su-de-sh <sudesh7443@gmail.com>
2022-06-08 11:10:57 +0545

    arrays-ii problem no 1 solved

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function evenAndOdd(array) {
+  let newArray = [[], []];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      newArray[1].push(array[i]);
+    } else {
+      newArray[0].push(array[i]);
+    }
+  }
+  return newArray;
+}
