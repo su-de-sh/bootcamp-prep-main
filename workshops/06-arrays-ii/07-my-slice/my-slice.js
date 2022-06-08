d7b82929d6c05c170e8315555175d09f069742f1
su-de-sh <sudesh7443@gmail.com>
2022-06-08 23:08:30 +0545

    array-ii 07 problem solved

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function mySlice(originalArray, startIdx = 0, endIdx = originalArray.length) {
+  debugger;
+  let newArray = [];
+  if (startIdx < 0) {
+    startIdx = originalArray.length + startIdx;
+  }
+  if (endIdx < 0) {
+    endIdx = originalArray.length + endIdx;
+  }
+  for (let i = startIdx; i < endIdx; i++) {
+    newArray.push(originalArray[i]);
+  }
+  return newArray;
+}
+console.log(mySlice(["bagel", "baguette", "bialy", "brioche"], -1, 2));
