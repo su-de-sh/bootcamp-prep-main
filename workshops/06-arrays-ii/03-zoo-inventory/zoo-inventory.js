4821c6b6cf1b46d8deb4c2a58f884e3d6f89d8c6
su-de-sh <sudesh7443@gmail.com>
2022-06-08 12:00:59 +0545

    array-ii 03 problem solved

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function zooInventory(array) {
+  newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(
+      array[i][0] + " the " + array[i][1][0] + " is " + array[i][1][1] + "."
+    );
+  }
+  return newArray;
+}
