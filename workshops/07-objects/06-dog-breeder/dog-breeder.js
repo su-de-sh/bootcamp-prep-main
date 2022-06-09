1b43e3f38bc8db37a120e071b0e5d253b32da4aa
su-de-sh <sudesh7443@gmail.com>
2022-06-09 14:43:27 +0545

    object problem no 06 solved

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function dogBreeder(name = "Steve", age = 0) {
+  let myObj = {};
+  if (typeof name === "string") {
+    myObj.name = name;
+    myObj.age = age;
+  } else {
+    myObj.name = "Steve";
+    myObj.age = name;
+  }
+  return myObj;
+}
