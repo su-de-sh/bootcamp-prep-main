d4b002094253b88b79506070440f2dd24c5a7cbc
su-de-sh <sudesh7443@gmail.com>
2022-06-09 10:59:55 +0545

    object problem no 1 solved

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function lastFridayNight(transactions) {
+  let totalAmount = 0;
+  for (let i = 0; i < transactions.length; i++) {
+    totalAmount += transactions[i].amount;
+  }
+  return totalAmount;
+}
