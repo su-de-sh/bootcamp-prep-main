415d2f1239aca81301d9faefc925bce1d32d0785
su-de-sh <sudesh7443@gmail.com>
2022-06-09 11:00:53 +0545

    objects problem no 1 solved

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+  let newArray = originalArray.splice(
+    originalArray.length - rotateNum,
+    rotateNum
+  );
+  return newArray.concat(originalArray);
+}
+
+console.log(rotateArray([1, 2, 3, 4, 5], 2));
