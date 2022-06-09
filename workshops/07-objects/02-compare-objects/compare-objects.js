61e3e96b53bc90294d34da60373cbfdcf5b22e3e
su-de-sh <sudesh7443@gmail.com>
2022-06-09 11:21:22 +0545

    object 02 problem solved

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function compareObjects(object1, object2) {
+  let lengthOfObjOne = Object.keys(object1).length;
+  let lengthOfObjTwo = Object.keys(object2).length;
+
+  console.log(lengthOfObjOne);
+  console.log(lengthOfObjTwo);
+  if (lengthOfObjOne !== lengthOfObjTwo) {
+    return false;
+  }
+  for (keys in object2) {
+    if (object2[keys] !== object1[keys]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+compareObjects({ name: "nick" }, { name: "nick" });
