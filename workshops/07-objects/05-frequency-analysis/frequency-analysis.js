6ddb887939013391355d5a459900b0f72d7dfdfb
su-de-sh <sudesh7443@gmail.com>
2022-06-09 14:24:49 +0545

    objects problem no 05 solved

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(str) {
+  str = str.toLowerCase();
+
+  let myObj = {};
+  debugger;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] in myObj) {
+      myObj[str[i]]++;
+    } else myObj[str[i]] = 1;
+  }
+
+  return myObj;
+}
