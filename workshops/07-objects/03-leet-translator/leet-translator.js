5b24a0b57ab6d773605dfa45d1729ad0471d7db0
su-de-sh <sudesh7443@gmail.com>
2022-06-09 12:34:25 +0545

    object leet translator problem solved

--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -1,4 +1,74 @@
-let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let leetChars = [
+  "@",
+  "8",
+  "(",
+  "|)",
+  "3",
+  "ph",
+  "g",
+  "#",
+  "l",
+  "_|",
+  "|<",
+  "1",
+  "|'|'|",
+  "//",
+  "0",
+  "|D",
+  "(,)",
+  "|2",
+  "5",
+  "+",
+  "|_|",
+  "|/",
+  "|/|/'",
+  "><",
+  "j",
+  "2",
+];
 
 // YOUR CODE BELOW
+
+function leetTranslator(str) {
+  str = str.toLowerCase();
+  let myStr = "";
+  let myLeetMap = {};
+  for (let i = 0; i < letters.length; i++) {
+    let letter = letters[i];
+    myLeetMap[letter] = leetChars[i];
+  }
+  for (let j = 0; j < str.length; j++) {
+    myStr += myLeetMap[str[j]];
+  }
+  return myStr;
+}
+
+leetTranslator("sudesh");
