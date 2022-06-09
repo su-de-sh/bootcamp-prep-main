bc866422b39a34374140bde709afb5dc2b04301f
su-de-sh <sudesh7443@gmail.com>
2022-06-09 16:04:39 +0545

    array-ii problem 06 solved

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,10 +1,15 @@
 // YOUR CODE BELOW
 function rotateArray(originalArray, rotateNum) {
-  let newArray = originalArray.splice(
-    originalArray.length - rotateNum,
-    rotateNum
-  );
-  return newArray.concat(originalArray);
+  if (rotateNum < 0) {
+    let newArray = originalArray.splice(0, -rotateNum);
+    return originalArray.concat(newArray);
+  } else {
+    let newArray = originalArray.splice(
+      originalArray.length - rotateNum,
+      rotateNum
+    );
+    return newArray.concat(originalArray);
+  }
 }
 
-console.log(rotateArray([1, 2, 3, 4, 5], 2));
+console.log(rotateArray([1, 2, 3, 4, 5], -2));
