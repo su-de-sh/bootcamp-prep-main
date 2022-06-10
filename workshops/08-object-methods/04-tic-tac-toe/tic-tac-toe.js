103e22b2811b1ce27929791c8685e441a19e924e
su-de-sh <sudesh7443@gmail.com>
2022-06-10 13:18:25 +0545

    object methods problem solved

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  move: function (character, rowNum, colNum) {
+    if (this.board[rowNum][colNum] === null) {
+      this.board[rowNum][colNum] = character;
+    }
+    console.log(this.board);
+    return this.board;
+  },
+  clear: function () {
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    return this.board;
+  },
+};
+// console.log(ticTacToe.board);
+// console.log(ticTacToe.move("s", 1, 1));
