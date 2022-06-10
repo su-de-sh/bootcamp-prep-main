c2ae6965d03a7cee758ce7c155da46bf81631e15
su-de-sh <sudesh7443@gmail.com>
2022-06-10 12:31:24 +0545

    object method problem no 03 solved

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,26 @@
-// YOUR CODE BELOW
+// YOUR CODE BELOW,
+function callThemAll(object, value) {
+  let myArray = [];
+  debugger;
+  for (let keys in object) {
+    if (typeof object[keys] === "function") {
+      myArray.push(object[keys](value));
+    }
+  }
+
+  return myArray;
+}
+
+// let addsNums = {
+//   addTen: function (num) {
+//     return num + 10;
+//   },
+
+//   addTwenty: function (num) {
+//     return num + 20;
+//   },
+
+//   someProperty: "value",
+// };
+
+// console.log(callThemAll({}, 100)); // => [110, 120]
