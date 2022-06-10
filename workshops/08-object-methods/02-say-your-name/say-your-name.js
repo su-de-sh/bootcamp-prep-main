d97f8be76c1387eaf5f4400d08636cd28c0a6ef9
su-de-sh <sudesh7443@gmail.com>
2022-06-10 11:07:11 +0545

    Object method problem 02 solved

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+let me2 = {
+  name: "Sudesh",
+  getGreeting: function (you) {
+    return `Hi ${you.name}, my name is ${this.name}.`;
+  },
+};
+
+let you = {
+  name: "Alyssa",
+};
