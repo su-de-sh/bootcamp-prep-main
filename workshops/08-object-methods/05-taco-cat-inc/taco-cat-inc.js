8dc9280c09e58d890a976fde693ab5ce25c3b304
su-de-sh <sudesh7443@gmail.com>
2022-06-10 15:34:03 +0545

    object method problem 05 solved

--- workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
+++ workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.js
@@ -1,25 +1,56 @@
 let tacoCatInc = {
   gourmetShell: {
-    'hard treat shell': {cost: 2, quantity: 100},
-    'soft treat shell': {cost: 1.5, quantity: 100}
+    "hard treat shell": { cost: 2, quantity: 100 },
+    "soft treat shell": { cost: 1.5, quantity: 100 },
   },
 
   gourmetFishFilling: {
-    'salmon': {cost: 5, quantity: 100},
-    'tuna': {cost: 5.5, quantity: 100},
-    'sardines': {cost: 1.5, quantity: 100}
+    salmon: { cost: 5, quantity: 100 },
+    tuna: { cost: 5.5, quantity: 100 },
+    sardines: { cost: 1.5, quantity: 100 },
   },
 
   gourmetVeggie: {
-    'cat grass': {cost: 1, quantity: 100}
+    "cat grass": { cost: 1, quantity: 100 },
   },
 
   gourmetSeasoning: {
-    'cat nip': {cost: 0.5, quantity: 100},
-    'treat dust': {cost: 0.1, quantity: 100}
+    "cat nip": { cost: 0.5, quantity: 100 },
+    "treat dust": { cost: 0.1, quantity: 100 },
   },
 
-  cash: 0
+  currentInventory: function () {
+    let total = 0;
+    for (let keys in this) {
+      for (let key in this[keys]) {
+        total += this[keys][key].cost * this[keys][key].quantity;
+      }
+    }
+    return total;
+  },
+
+  sale: function (odrs) {
+    if (Object.keys(odrs).length === 0) return 0;
+    let totalSale = 0;
+    for (let keys in odrs) {
+      let item = odrs[keys];
+      let costOfItem = this[keys][item].cost;
+      this[keys][item].quantity -= 1;
+
+      totalSale += costOfItem;
+    }
+    this.cash += totalSale;
+
+    return totalSale;
+  },
+
+  cash: 0,
 };
 
 // YOUR CODE BELOW
+// let order = {
+//   gourmetShell: "hard treat shell",
+//   gourmetFishFilling: "salmon",
+// };
+// tacoCatInc.sale(order);
+// console.log(tacoCatInc.cash);
--- workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.spec.js
+++ workshops/08-object-methods/05-taco-cat-inc/taco-cat-inc.spec.js
@@ -1,31 +1,28 @@
-
-describe('tacoCatInc', () => {
-
-  it('is an object', () => {
-    expect(typeof tacoCatInc).toEqual('object');
+describe("tacoCatInc", () => {
+  it("is an object", () => {
+    expect(typeof tacoCatInc).toEqual("object");
   });
 
-  describe('currentInventory', () => {
-
-    it('is a method in tacoCatInc', () => {
-      expect('currentInventory' in tacoCatInc).toEqual(true);
+  describe("currentInventory", () => {
+    it("is a method in tacoCatInc", () => {
+      expect("currentInventory" in tacoCatInc).toEqual(true);
     });
 
-    it('is a function', () => {
-      expect(typeof tacoCatInc.currentInventory).toEqual('function');
+    it("is a function", () => {
+      expect(typeof tacoCatInc.currentInventory).toEqual("function");
     });
 
-    it('returns a number', () => {
+    it("returns a number", () => {
       let returnedValue = tacoCatInc.currentInventory();
-      expect(typeof returnedValue).toEqual('number');
+      expect(typeof returnedValue).toEqual("number");
     });
 
-    it('returns the correct value of the inventory', () => {
+    it("returns the correct value of the inventory", () => {
       let returnedValue = tacoCatInc.currentInventory();
       expect(returnedValue).toEqual(1710);
     });
 
-    it('does not include cash in the returned value', () => {
+    it("does not include cash in the returned value", () => {
       tacoCatInc.cash = 1000;
 
       let returnedValue = tacoCatInc.currentInventory();
@@ -33,13 +30,10 @@ describe('tacoCatInc', () => {
 
       tacoCatInc.cash = 0;
     });
-
   });
 
-  describe('sale', () => {
-
+  describe("sale", () => {
     beforeEach(() => {
-
       // before each test, reset the cash and inventory quantities in tacoCatInc
       resetValues();
 
@@ -52,66 +46,61 @@ describe('tacoCatInc', () => {
 
         tacoCatInc.cash = 0;
       }
-
     });
 
-    it('is a method in tacoCatInc', () => {
-      expect('sale' in tacoCatInc).toEqual(true);
+    it("is a method in tacoCatInc", () => {
+      expect("sale" in tacoCatInc).toEqual(true);
     });
 
-    it('is a function', () => {
-      expect(typeof tacoCatInc.sale).toEqual('function');
+    it("is a function", () => {
+      expect(typeof tacoCatInc.sale).toEqual("function");
     });
 
-    it('returns a number', () => {
+    it("returns a number", () => {
       let returnedValue = tacoCatInc.sale({});
-      expect(typeof returnedValue).toEqual('number');
+      expect(typeof returnedValue).toEqual("number");
     });
 
-    it('returns the correct total price for the sale', () => {
+    it("returns the correct total price for the sale", () => {
       let order = {
-        gourmetShell: 'hard treat shell',
-        gourmetFishFilling: 'salmon'
-      }
+        gourmetShell: "hard treat shell",
+        gourmetFishFilling: "salmon",
+      };
 
       let returnedValue = tacoCatInc.sale(order);
       expect(returnedValue).toEqual(7);
     });
 
-    it('updates the inventory quantities for the correct items', () => {
+    it("updates the inventory quantities for the correct items", () => {
       let order = {
-        gourmetShell: 'soft treat shell',
-        gourmetFishFilling: 'tuna',
-        gourmetVeggie: 'cat grass',
-        gourmetSeasoning: 'treat dust'
-      }
+        gourmetShell: "soft treat shell",
+        gourmetFishFilling: "tuna",
+        gourmetVeggie: "cat grass",
+        gourmetSeasoning: "treat dust",
+      };
 
       tacoCatInc.sale(order);
 
-      expect(tacoCatInc.gourmetShell['soft treat shell'].quantity).toEqual(99);
-      expect(tacoCatInc.gourmetShell['hard treat shell'].quantity).toEqual(100);
+      expect(tacoCatInc.gourmetShell["soft treat shell"].quantity).toEqual(99);
+      expect(tacoCatInc.gourmetShell["hard treat shell"].quantity).toEqual(100);
       expect(tacoCatInc.gourmetFishFilling.salmon.quantity).toEqual(100);
       expect(tacoCatInc.gourmetFishFilling.tuna.quantity).toEqual(99);
       expect(tacoCatInc.gourmetFishFilling.sardines.quantity).toEqual(100);
-      expect(tacoCatInc.gourmetVeggie['cat grass'].quantity).toEqual(99);
-      expect(tacoCatInc.gourmetSeasoning['cat nip'].quantity).toEqual(100);
-      expect(tacoCatInc.gourmetSeasoning['treat dust'].quantity).toEqual(99);
-
+      expect(tacoCatInc.gourmetVeggie["cat grass"].quantity).toEqual(99);
+      expect(tacoCatInc.gourmetSeasoning["cat nip"].quantity).toEqual(100);
+      expect(tacoCatInc.gourmetSeasoning["treat dust"].quantity).toEqual(99);
     });
 
-
-    it('updates the cash property by the correct amount', () => {
+    it("updates the cash property by the correct amount", () => {
       let order = {
-        gourmetShell: 'hard treat shell',
-        gourmetFishFilling: 'sardines',
-        gourmetSeasoning: 'treat dust'
-      }
+        gourmetShell: "hard treat shell",
+        gourmetFishFilling: "sardines",
+        gourmetSeasoning: "treat dust",
+      };
 
       tacoCatInc.sale(order);
 
       expect(tacoCatInc.cash).toEqual(3.6);
     });
-
   });
-
 });
