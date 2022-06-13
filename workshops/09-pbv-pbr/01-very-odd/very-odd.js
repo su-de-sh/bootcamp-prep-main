f164778d11afda0604be4118e99d86a31254e0b3
su-de-sh <sudesh7443@gmail.com>
2022-06-13 11:13:19 +0545

    pvrpbr problem 01 solved

--- workshops/09-pbv-pbr/01-very-odd/very-odd.js
+++ workshops/09-pbv-pbr/01-very-odd/very-odd.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function veryOdd(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 1) {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+}
