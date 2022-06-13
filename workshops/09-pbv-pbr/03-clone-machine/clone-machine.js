80652e4fec18388783957cd73b9a008d82ac2ed4
su-de-sh <sudesh7443@gmail.com>
2022-06-13 12:12:16 +0545

    pbvpbr problem 03 solved

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function cloneMachine(animal) {
+  let animalClone = {};
+
+  animalClone.name = animal.name + "Clone";
+  animalClone.species = animal.species;
+  animalClone.offspring = [];
+  animal.offspring.push(animalClone.name);
+  return animalClone;
+}
