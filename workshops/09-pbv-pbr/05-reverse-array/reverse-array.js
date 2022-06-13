290df36e30b5ee2432ca67bff3de999c4317bac2
su-de-sh <sudesh7443@gmail.com>
2022-06-13 14:59:14 +0545

    pbrpbv 05 problem solved

--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+function reverseArray(myArray) {
+  let requiredArray = [];
+  for (let i = myArray.length - 1; i >= 0; i--) {
+    requiredArray.push(myArray[i]);
+  }
+
+  while (myArray.length) {
+    myArray.pop();
+  }
+
+  for (let i = 0; i < requiredArray.length; i++) {
+    myArray.push(requiredArray[i]);
+  }
+  return myArray;
+}
