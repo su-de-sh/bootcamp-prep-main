c78127a82bf3c1c734f4e018c0df96b52791887e
su-de-sh <sudesh7443@gmail.com>
2022-06-13 11:18:19 +0545

    pbrpbv problem 02 solved

--- workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
+++ workshops/09-pbv-pbr/02-very-odd-mutant/very-odd-mutant.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function veryOddMutant(array) {
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === 0) {
+      array[i] = "normie";
+      count++;
+    }
+  }
+
+  return count;
+}
