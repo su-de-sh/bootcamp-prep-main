5676d4c65d4c8fba319f96729fece02ee0535c49
su-de-sh <sudesh7443@gmail.com>
2022-06-13 16:16:29 +0545

    pbrpbv 06 problem solved

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function deeperCopy(array) {
+  let requiredArrray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      let tempArray = [];
+      for (let j = 0; j < array[i].length; j++) {
+        tempArray.push(array[i][j]);
+      }
+      requiredArrray.push(tempArray);
+    } else {
+      requiredArrray.push(array[i]);
+    }
+  }
+  return requiredArrray;
+}
