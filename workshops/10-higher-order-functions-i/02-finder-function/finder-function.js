d1bcce8cca26d00886a9443b10f5717fa25b0a5f
su-de-sh <sudesh7443@gmail.com>
2022-06-15 11:27:46 +0545

    higer order function probelm 02 solved

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function finderFunction(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    let result = callback(array[i]);
+
+    if (result) return i;
+  }
+  return -1;
+}
+let numbers = [1, 3, 5, 64, 7, 12];
+let odds = [9, 13, 15, 17];
+
+function isEven(num) {
+  return !(num % 2);
+}
+
+// console.log(finderFunction(numbers, isEven));
