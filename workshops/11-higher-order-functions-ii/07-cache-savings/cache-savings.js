9b54acced910d1be35f8a0acc13ca902545216ff
su-de-sh <sudesh7443@gmail.com>
2022-06-15 15:04:00 +0545

    higher order functions -ii problem 07 solved

--- workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
+++ workshops/11-higher-order-functions-ii/07-cache-savings/cache-savings.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function cacheSavings(callback) {
+  let myCache = {};
+  function newFunc(arg) {
+    if (arg in myCache) {
+      return myCache[arg];
+    } else {
+      myCache[arg] = callback(arg);
+      return myCache[arg];
+    }
+  }
+  return newFunc;
+}
