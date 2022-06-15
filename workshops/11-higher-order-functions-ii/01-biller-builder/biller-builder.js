2725825c9dff59166b60dc1e452fb86e03054098
su-de-sh <sudesh7443@gmail.com>
2022-06-15 13:30:17 +0545

    higher order function -ii problem 01 solved

--- workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
+++ workshops/11-higher-order-functions-ii/01-biller-builder/biller-builder.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function billerBuilder(state) {
+  if (state === "NY") {
+    function newYorkBiller(price) {
+      return price * 1.03 * 1.04;
+    }
+    return newYorkBiller;
+  }
+  if (state === "NJ") {
+    function newJersBiller(price) {
+      return price * 1.05 * 1.06625;
+    }
+    return newJersBiller;
+  }
+}
