602dceb0f9a9dfb283bc72f845a07e8bc1062bed
su-de-sh <sudesh7443@gmail.com>
2022-06-15 11:38:31 +0545

    higher order function problem 03 solved

--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function whosASpecial(petsArray) {
+  let myStr = "";
+  petsArray.forEach((element) => {
+    myStr += `${element.name} the ${element.species} is very special! `;
+  });
+
+  return myStr.trim();
+}
