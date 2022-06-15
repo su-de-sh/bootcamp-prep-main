623f5a894f238144b7dab0a7faa0f1791ad2d767
su-de-sh <sudesh7443@gmail.com>
2022-06-15 14:52:01 +0545

    higher order function -ii problem 06 solved

--- workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
+++ workshops/11-higher-order-functions-ii/06-get-dog-breeder/get-dog-breeder.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function getDogBreeder(name, age) {
+  function dogBreeder(name1 = name, age1 = age) {
+    if (typeof name1 === "number") {
+      return { name: name, age: name1 };
+    }
+    return { name: name1, age: age1 };
+  }
+
+  return dogBreeder;
+}
